ed153b718ca7b9f0d7ba491a9c90993fa0c680b7
Adam Corriveau <39892162+livemonkey1300@users.noreply.github.com>
2025-11-05 00:35:29 +0000

    Add Google Compute Instance resource configuration in Terraform

--- google-vm-small/main.tf
+++ google-vm-small/main.tf
@@ -20,3 +20,36 @@ provider "google" {
   region      = var.region
   zone        = var.zone
 }
+
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = var.tags
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.vm_image.self_link
+    }
+  }
+
+  network_interface {
+    network = var.network
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "terraform:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  labels = {
+    environment = "dev"
+    project     = "multi-project-terraform"
+  }
+}
