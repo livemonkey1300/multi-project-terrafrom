dd248901fb3a00df698509c1d228656830e29988
Adam Corriveau <39892162+livemonkey1300@users.noreply.github.com>
2025-11-05 00:19:19 +0000

    Add provider configuration and define variables for GCP project, region, and zone

--- google-vm-small/main.tf
+++ google-vm-small/main.tf
@@ -14,3 +14,9 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
---
+++ google-vm-small/var.tf
@@ -0,0 +1,52 @@
+variable "project_id" {
+    description = "The GCP project ID"
+    type        = string
+}
+
+variable "region" {
+    description = "The GCP region"
+    type        = string
+    default     = "northamerica-northeast1"
+}
+
+variable "zone" {
+    description = "The GCP zone"
+    type        = string
+    default     = "northamerica-northeast1-a"
+}
+
+variable "instance_name" {
+    description = "Name of the VM instance"
+    type        = string
+    default     = "small-vm"
+}
+
+variable "machine_type" {
+    description = "Machine type for the VM"
+    type        = string
+    default     = "e2-micro"
+}
+
+variable "image_family" {
+    description = "Image family for the VM"
+    type        = string
+    default     = "ubuntu-2004-lts"
+}
+
+variable "image_project" {
+    description = "Project containing the image"
+    type        = string
+    default     = "ubuntu-os-cloud"
+}
+
+variable "network" {
+    description = "Network for the VM"
+    type        = string
+    default     = "default"
+}
+
+variable "tags" {
+    description = "Network tags for the VM"
+    type        = list(string)
+    default     = []
+}
